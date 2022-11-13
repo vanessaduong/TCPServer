689ef31f7f98c0395557e2f3902c8967ecf5dc71
Nhat Tuong Van DUONG <vanduong@iMac-de-Nhat.local>
2022-11-13 15:45:41 +0100

    Add TimeHelper and Utils classes Ignore SIGPIPE

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TCPServer
-Simple TCP server C++
+
+Description
+- Handles up to 6 TCP clients
+- New thread spawned for each new client
+- Unique id created every second and sent to client
+- Prints number of clients connecting when receiving new line character
+- "Ctrl C" to exit server
+
+How to compile
+- MacOS: g++ -std=c++17 server.cpp -o server
+- Linux: g++ -Wall -std=c++17 server.cpp -o server -lpthread
--- server.cpp
+++ server.cpp
@@ -1,54 +1,104 @@
-#include <iostream>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <string>
-#include <thread>
-#include <vector>
-#include <mutex>
-#include <ctime>
-#define MAX_ClIENT_ALLOWED 2
+#include "iostream"
+#include "sys/types.h"
+#include "unistd.h"
+#include "sys/socket.h"
+#include "netdb.h"
+#include "arpa/inet.h"
+#include "string"
+#include "thread"
+#include "vector"
+#include "mutex"
+#include "ctime"
+#include "chrono"
+#include "signal.h"
+#include "algorithm"
+
+#define MAX_ClIENT_ALLOWED 6
 
 bool running{true};
-std::mutex m;
+std::mutex openedTCPmutex;
+unsigned int openedTCP=0;
 
-void getId(int& fileDescriptor)
+class TimeHelper
 {
-	time_t seconds;
-	char buffer[200] = { 0 };
-	while (running)
+private:
+	std::chrono::high_resolution_clock::time_point start{std::chrono::high_resolution_clock::now()};
+public:
+	auto awake_time() {
+	    using std::chrono::operator""ms;
+	    return start + 1000ms;
+	}
+};
+
+
+class Utils
+{
+public:
+	static void threadActions(int& fileDescriptor)
 	{
-		//m.lock();
-		seconds = time (NULL);
-		int uniqueId = seconds + fileDescriptor*100000;
-		//m.unlock();
-		int len = snprintf(buffer, sizeof(buffer), "%d\n", uniqueId);
-		int ret = send(fileDescriptor, buffer, len+1, 0);
-		if (ret != len+1)
+		timeval selectTimeVal;
+	    selectTimeVal.tv_sec=0;
+	    fd_set rset;
+
+	    openedTCPmutex.lock();
+	    openedTCP++;
+	    openedTCPmutex.unlock();
+
+		unsigned int increment = (unsigned int)time (NULL) + (unsigned int)fileDescriptor*100000;
+	    char buffer[200] = { 0 };
+		while (running)
 		{
-			fileDescriptor = 0;
-			return;
+			int ret;
+	        /// write
+	        int len = snprintf(buffer, sizeof(buffer), "%d\n", increment);
+	        ret = send(fileDescriptor, buffer, len+1, 0);
+	        TimeHelper start;
+	        if (ret != len+1)
+	        {
+	            openedTCPmutex.lock();
+	            openedTCP--;
+	            openedTCPmutex.unlock();
+	            fileDescriptor = 0;
+	            return;
+	        }
+	        /// read
+	        selectTimeVal.tv_usec=100000;
+	        FD_SET(fileDescriptor, &rset);
+	        ret = select(fileDescriptor+1, &rset, NULL, NULL, &selectTimeVal);
+	        if (FD_ISSET(fileDescriptor, &rset) && ret>0) {
+	            bool backslashN=false;
+	            char buf[4096];
+	            int bytesRecv = recv(fileDescriptor, buf, 4096, 0);
+	            for (int i=0;i<bytesRecv;i++) {
+	                if (buf[i]=='\n') {backslashN=true;break;}
+	            }
+	            if (backslashN) {
+	                int len2 = snprintf(buffer, sizeof(buffer), "%d TCP connection currently active\n", openedTCP);
+	                send(fileDescriptor, buffer, len2+1, MSG_NOSIGNAL);
+	            }
+	        }
+	        /// increment ID and sleep to the next second
+	        increment++;
+	        std::this_thread::sleep_until(start.awake_time());
 		}
-		uniqueId++;
-		sleep(1);
+		send(fileDescriptor, "Thank you\n", 20, MSG_NOSIGNAL);
+	    close(fileDescriptor);
+	    fileDescriptor=0;
+
+	    openedTCPmutex.lock();
+	    openedTCP--;
+	    openedTCPmutex.unlock();
 	}
-	send(fileDescriptor, "Thank you\n", 10, 0);
-    close(fileDescriptor);
-    fileDescriptor=0;
-}
 
-
-void signalHandler(int signalNum)
-{
-	if (signalNum == SIGINT)
+	static void signalHandler(int signalNum)
 	{
-		//std::cout << "Thank you." << std::endl;
-		running = false;
+		if (signalNum == SIGINT)
+		{
+			running = false;
+		}
 	}
-}
+};
+
 
 int main()
 {
@@ -56,7 +106,7 @@ int main()
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1)
     {
-        std::cerr << "Can't create a socket!";
+        //std::cerr << "Can't create a socket!";
         return -1;
     }
 
@@ -67,48 +117,59 @@ int main()
 
     if (bind(listening, (struct sockaddr *)&hint, sizeof(hint)) == -1) 
     {
-        std::cerr << "Can't bind to IP/port";
+        //std::cerr << "Can't bind to IP/port";
         return -2;
     }
 
     if (listen(listening, SOMAXCONN) == -1)
     {
-        std::cerr << "Can't listen !";
+        //std::cerr << "Can't listen !";
         return -3;
     }
 
     sockaddr_in client;
     socklen_t clientSize = sizeof(client);
 
-    signal(SIGINT, signalHandler);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, Utils::signalHandler);
 
-	//int fileDescriptor[6] = {};
 	int fileDescriptors[MAX_ClIENT_ALLOWED] = {};
+	fd_set rset;
+	timeval selectTimeVal;
+	selectTimeVal.tv_sec=0;
+    int ret;
 	while (running)
 	{
-		int clientSocket = accept(listening, (struct sockaddr *)&client, &clientSize);
-
-		if (std::find(std::begin(fileDescriptors), std::end(fileDescriptors), 0) != std::end(fileDescriptors))
-		{
-			for (int i = 0; i < MAX_ClIENT_ALLOWED; i++)
+		selectTimeVal.tv_usec=100000;
+        FD_SET(listening, &rset);
+        ret = select(listening+1, &rset, NULL, NULL, &selectTimeVal);
+        if (FD_ISSET(listening, &rset) && ret>0)
+        {
+        	int clientSocket = accept(listening, (struct sockaddr *)&client, &clientSize);
+			if (std::find(std::begin(fileDescriptors), std::end(fileDescriptors), 0) != std::end(fileDescriptors))
 			{
-				if (fileDescriptors[i] == 0)
+				for (int i = 0; i < MAX_ClIENT_ALLOWED; i++)
 				{
-					fileDescriptors[i] = clientSocket;
-					std::thread th (getId, std::ref(fileDescriptors[i]));
-					th.detach();
-					break;
+					if (fileDescriptors[i] == 0)
+					{
+						fileDescriptors[i] = clientSocket;
+						std::thread th (&Utils::threadActions, std::ref(fileDescriptors[i]));
+						th.detach();
+						break;
+					}
+					
 				}
-				
 			}
-		}
-		else
-		{
-			send(clientSocket, "Max connexions reached.\n", 24, 0);
-            close(clientSocket);
-		}
+			else
+			{
+				send(clientSocket, "Max connexions reached.\n", 24, MSG_NOSIGNAL);
+	            close(clientSocket);
+			}
+        }	
+		
 	}
 
+	while (openedTCP>0) usleep(5000);
 	close(listening);
 
     return 0;
