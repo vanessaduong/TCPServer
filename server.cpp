b5b5c32b95d066da2d27a7788aac9f65473f958f
Nhat Tuong Van DUONG <vanduong@iMac-de-Nhat.local>
2022-11-13 09:17:33 +0100

    Add uniqueID using time_t Add check max connexions reached.

--- server.cpp
+++ server.cpp
@@ -10,22 +10,20 @@
 #include <vector>
 #include <mutex>
 #include <ctime>
+#define MAX_ClIENT_ALLOWED 2
 
 bool running{true};
 std::mutex m;
-int uniqueId{0};
-const int MAX_ClIENT_ALLOWED{6};
 
 void getId(int& fileDescriptor)
 {
 	time_t seconds;
-	//int increment = seconds + fileDescriptor*100000;
 	char buffer[200] = { 0 };
 	while (running)
 	{
 		//m.lock();
 		seconds = time (NULL);
-		uniqueId = seconds + fileDescriptor*100000 +1;
+		int uniqueId = seconds + fileDescriptor*100000;
 		//m.unlock();
 		int len = snprintf(buffer, sizeof(buffer), "%d\n", uniqueId);
 		int ret = send(fileDescriptor, buffer, len+1, 0);
@@ -34,8 +32,12 @@ void getId(int& fileDescriptor)
 			fileDescriptor = 0;
 			return;
 		}
+		uniqueId++;
 		sleep(1);
 	}
+	send(fileDescriptor, "Thank you\n", 10, 0);
+    close(fileDescriptor);
+    fileDescriptor=0;
 }
 
 
@@ -50,9 +52,7 @@ void signalHandler(int signalNum)
 
 int main()
 {
-    std::vector<std::thread> threads;
 
-    std::cout << "Creating server socket..." << std::endl;
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1)
     {
@@ -65,14 +65,12 @@ int main()
     hint.sin_port = htons(54000);
     inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 
-    std::cout << "Binding socket to sockaddr..." << std::endl;
     if (bind(listening, (struct sockaddr *)&hint, sizeof(hint)) == -1) 
     {
         std::cerr << "Can't bind to IP/port";
         return -2;
     }
 
-    std::cout << "Mark the socket for listening..." << std::endl;
     if (listen(listening, SOMAXCONN) == -1)
     {
         std::cerr << "Can't listen !";
@@ -82,71 +80,36 @@ int main()
     sockaddr_in client;
     socklen_t clientSize = sizeof(client);
 
-    std::cout << "Accept client call..." << std::endl;
     signal(SIGINT, signalHandler);
 
-	int fileDescriptor[6] = {};
+	//int fileDescriptor[6] = {};
+	int fileDescriptors[MAX_ClIENT_ALLOWED] = {};
 	while (running)
 	{
 		int clientSocket = accept(listening, (struct sockaddr *)&client, &clientSize);
-		int i;
-		for (i = 0; i < MAX_ClIENT_ALLOWED; i++)
+
+		if (std::find(std::begin(fileDescriptors), std::end(fileDescriptors), 0) != std::end(fileDescriptors))
 		{
-			if (fileDescriptor[i] == 0)
+			for (int i = 0; i < MAX_ClIENT_ALLOWED; i++)
 			{
-				fileDescriptor[i] = clientSocket;
-				std::thread th (getId, std::ref(fileDescriptor[i]));
-				th.detach();
-				break;
-			}
-			if (i >= MAX_ClIENT_ALLOWED)
-			{
-				send(clientSocket, "No more connexion allowed.", 12, 0);
-				close(clientSocket);
+				if (fileDescriptors[i] == 0)
+				{
+					fileDescriptors[i] = clientSocket;
+					std::thread th (getId, std::ref(fileDescriptors[i]));
+					th.detach();
+					break;
+				}
+				
 			}
 		}
+		else
+		{
+			send(clientSocket, "Max connexions reached.\n", 24, 0);
+            close(clientSocket);
+		}
 	}
-	
+
 	close(listening);
-	//close(clientSocket);
-
-/*
-    std::cout << "Received call..." << std::endl;
-    if (clientSocket == -1)
-    {
-        std::cerr << "Problem with client connecting!";
-        return -4;
-    }
-
-    std::cout << "Client address: " << inet_ntoa(client.sin_addr) << " and port: " << client.sin_port << std::endl;
-
-    close(listening);
-
-    char buf[4096];
-    while (true) {
-        // clear buffer
-        memset(buf, 0, 4096);
-
-        // wait for a message
-        int bytesRecv = recv(clientSocket, buf, 4096, 0);
-        if (bytesRecv == -1)
-        {
-            std::cerr << "There was a connection issue." << std::endl;
-        }
-        if (bytesRecv == 0)
-        {
-            std::cout << "The client disconnected" << std::endl;
-        }
-        
-        // display message
-        //std::cout << "Received: " << std::string(buf, 0, bytesRecv);
-
-        // return message
-        //send(clientSocket, buf, bytesRecv+1, 0);
-    	
-    }
-
-    close(clientSocket);*/
 
     return 0;
 
